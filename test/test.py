7fdda1f9fb2913ca33317c7afe2ad0ae38972158
p208p2002 <p208p2002@gmail.com>
2021-09-11 17:26:45 +0800

    Update test

--- test/test.py
+++ test/test.py
@@ -4,17 +4,15 @@ sys.path.append('../')
 from TagReconstruct import SearchInContext,SearchNearby
 
 context = """
-123456王小明是公司實際負責人7890
+王小明是公司實際負責人
 """
 search = SearchNearby('position_list.txt')
-new_tag = search.run(context=context,start_at=15,end_at=18,bound=3)
-print(context[15:18],new_tag) # 負責人, 實際負責人
+new_tag = search.run(context=context,start_at=9,end_at=12,bound=3)
+print(context[9:12],new_tag) # 負責人, 實際負責人
 
 
 context = """
-炎隆
-某某公司
-炎隆鐵工廠有限公司
+炎隆鐵工廠有限公司下稱炎隆
 """
 search = SearchInContext('company_list.txt')
 new_tag = search.run(context=context,tag='炎隆',bound=10)
